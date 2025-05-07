61c0dcdf92d26496f643ec94bf2aadfb46c89655
Luke Wriglesworth <luke.wriglesworth@mail.utoronto.ca>
2025-05-07 10:04:10 -0400

    ...

--- flake.nix
+++ flake.nix
@@ -5,17 +5,19 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     fenix.url = "github:nix-community/fenix";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
   };
   outputs = {
     flake-utils,
     nixpkgs,
     fenix,
+    nixpkgs-python,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShells = {
         rust = import ./shells/rust.nix {inherit system nixpkgs fenix;};
-        python = import ./shells/python.nix {inherit system nixpkgs;};
+        python = import ./shells/python.nix {inherit system nixpkgs nixpkgs-python;};
         latex = import ./shells/LaTeX.nix {inherit system nixpkgs;};
         haskell = import ./shells/haskell.nix {inherit system nixpkgs;};
         c = import ./shells/cpp.nix {inherit system nixpkgs;};
--- shells/python.nix
+++ shells/python.nix
@@ -1,37 +1,32 @@
 {
   nixpkgs,
+  nixpkgs-python,
   system,
   ...
 }: let
   pkgs = import nixpkgs {
     inherit system;
-    #config.replaceStdenv = {pkgs, ...}: pkgs.fastStdenv;
     config.allowUnfree = true;
   };
-  pythonFast = pkgs.python313Full.override {
-    enableOptimizations = true;
-    enableLTO = true;
-    reproducibleBuild = false;
-  };
-  pypkgs = pkgs.python313Packages;
+  python = pkgs.python313Full;
+  pythonPkgs = pkgs.python313Packages;
 in
   pkgs.mkShell {
     name = "python-venv";
     venvDir = "./.venv";
     nativeBuildInputs = [pkgs.undetected-chromedriver pkgs.google-chrome];
     buildInputs = [
-      pythonFast
+      python
       pkgs.nodejs
-      pypkgs.matplotlib
-      pypkgs.ipykernel
-      pypkgs.numpy
-      pypkgs.beautifulsoup4
-      pypkgs.pandas
-      pypkgs.requests
-      pypkgs.openpyxl
-      pypkgs.selenium
-      pypkgs.webdriver-manager
-      pypkgs.undetected-chromedriver
-      pypkgs.openai
+      pythonPkgs.ipykernel
+      pythonPkgs.numpy
+      pythonPkgs.beautifulsoup4
+      pythonPkgs.pandas
+      pythonPkgs.requests
+      pythonPkgs.openpyxl
+      pythonPkgs.selenium
+      pythonPkgs.webdriver-manager
+      pythonPkgs.undetected-chromedriver
+      pythonPkgs.openai
     ];
   }
